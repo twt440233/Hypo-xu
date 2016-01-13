705948c7c214d36f20dfe6ac19cf7e5c97c391c6
lanou <lan@lanoudeMac-mini-100.local>
2016-01-13 15:19:34 +0800

    添加工程

---
+++ ceshi/ceshi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ceshi
+//
+//  Created by lanou on 16/1/13.
+//  Copyright © 2016年 xuhaibo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ceshi/ceshi/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  ceshi
+//
+//  Created by lanou on 16/1/13.
+//  Copyright © 2016年 xuhaibo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"123");
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
